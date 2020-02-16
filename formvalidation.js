f511e6c683dd70891c8ad39d682e4752660f02ff
geoffreysena <60253048+geoffreysena@users.noreply.github.com>
2020-02-16 23:13:36 +0800

    Add files via upload

---
+++ formvalidation.js
@@ -0,0 +1,13 @@
+function myFunction() {
+    var x = document.getElementById("title");
+    x.value = x.value.toUpperCase();
+  }
+
+function yesSubmit() {
+    alert("Thank you for choosing us!")
+}
+
+function noSubmit() {
+    alert("Feel free to choose us again!")
+}
+
